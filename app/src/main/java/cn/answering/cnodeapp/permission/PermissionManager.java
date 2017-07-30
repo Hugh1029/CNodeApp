ca12835a773dcca0c66a2d5e3c218a1dfc766eb3
exploreAnswer <34948062@qq.com>
2017-07-30 13:20:18 +0800

    运行时权限申请的封装

---
+++ app/src/main/java/cn/answering/cnodeapp/permission/PermissionManager.java
@@ -0,0 +1,68 @@
+package cn.answering.cnodeapp.permission;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zjp on 2017/7/29.
+ * 运行权限申请管理
+ */
+public class PermissionManager {
+    private static final String TAG = "PermissionManager";
+
+    /**
+     *
+     * @param object Activity
+     * @param requestCode  申请权限时的Code码
+     * @param listener  如果申请的权限已经同意，调用监听中相应的函数
+     * @param permissions   申请的权限列表
+     */
+    public void perssionManagerRequest(Object object,int requestCode,PermissionManagerListener listener,String... permissions ){
+        List<String> permissionsList = new ArrayList<String>();
+        Activity act = getActivity(object);
+
+        for (int i = 0;i < permissions.length;i ++){
+            if (ContextCompat.checkSelfPermission(act,permissions[i]) != PackageManager.PERMISSION_GRANTED){
+                permissionsList.add(permissions[i]);
+            }
+        }
+
+        if (!permissionsList.isEmpty()){
+            String[] permissionArray = permissionsList.toArray(new String[permissionsList.size()]);
+            requestPermisssion(object,requestCode,permissionArray);
+        }else {
+            listener.allPermisionsAllow();
+        }
+    }
+
+    //获取activity参数
+    private Activity getActivity(Object object){
+        if (object instanceof  Activity){
+            return (Activity)object;
+        }else if (object instanceof  Fragment){
+            return ((Fragment) object).getActivity();
+        }
+        return null;
+    }
+
+    //根据不同情况申请权限
+    private void requestPermisssion(Object object,int requestCode,String... permissions){
+        if (object instanceof Activity){
+            ActivityCompat.requestPermissions((Activity) object,permissions,requestCode);
+        }else if (object instanceof Fragment){
+            ((Fragment) object).requestPermissions(permissions,requestCode);
+        }else {
+            throw new IllegalArgumentException(object.getClass().getName() + " is not supported");
+        }
+    }
+
+}
---
+++ app/src/main/java/cn/answering/cnodeapp/permission/PermissionManagerListener.java
@@ -0,0 +1,11 @@
+package cn.answering.cnodeapp.permission;
+
+/**
+ * Created by zjp on 2017/7/29.
+ * 所有权限都已经授权的监听器
+ */
+
+public interface PermissionManagerListener {
+
+    public void allPermisionsAllow();
+}
